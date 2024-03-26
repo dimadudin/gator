c47e13a59d972d69b631be495276d779a3faf103
dimadudin <dmitriy.i.dudin@gmail.com>
2024-03-26 20:28:52 +0300

    boilerplate for websocket endpoint

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/dimadudin/gator
 
 go 1.22.0
+
+require github.com/joho/godotenv v1.5.1
+
+require (
+	github.com/gorilla/websocket v1.5.1 // indirect
+	golang.org/x/net v0.17.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,19 +3,53 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	ws "github.com/gorilla/websocket"
+	"github.com/joho/godotenv"
 )
 
-func showWelcome(w http.ResponseWriter, r *http.Request) {
+var wsUpgrader = ws.Upgrader{
+	HandshakeTimeout: 20 * time.Second,
+	ReadBufferSize:   1024,
+	WriteBufferSize:  1024,
+}
+
+func serveWelcomePage(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(`Welcome to Chadder!`))
 }
 
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("GET /", showWelcome)
-	server := http.Server{
-		Addr:    ":8080",
-		Handler: mux,
+func serveWebSocketPage(w http.ResponseWriter, r *http.Request) {
+	conn, err := wsUpgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+		return
 	}
+	go writePump(conn)
+	go readPump(conn)
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("couldn't open .env file")
+	}
+
+	port := os.Getenv("PORT")
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", serveWelcomePage)
+	mux.HandleFunc("GET /ws", serveWebSocketPage)
+
+	server := http.Server{
+		Addr:              ":" + port,
+		Handler:           mux,
+		ReadTimeout:       3 * time.Second,
+		ReadHeaderTimeout: 3 * time.Second,
+	}
+
 	log.Fatal(server.ListenAndServe())
 }
